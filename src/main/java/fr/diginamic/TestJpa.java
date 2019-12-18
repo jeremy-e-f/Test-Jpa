c66c122e12db33d14550e27c467fc5881221ce2c
Jeremy FROMINVILLE <j.e.fromin@gmail.com>
2019-12-18 10:50:13 +0100

    JPA TP n°1 connexion à une base avec JPA

---
+++ src/main/java/fr/diginamic/TestJpa.java
@@ -0,0 +1,16 @@
+package fr.diginamic;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class TestJpa {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("pu_essai");
+		EntityManager em = entityManagerFactory.createEntityManager();
+
+	}
+
+}
